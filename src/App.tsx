739c48b3c726212c7bc8f2dd7a17a8b760ff9067
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-12-22 18:40:01 +0600

    project initialized with basic setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App:React.FC = () => {
+  return (
+    <main>
+      <h3 className='text-center'>Hello Tetris</h3>
+    </main>
+  );
+};
+
+export default App;
